dc61a4b93b9ec0d6b7f85c16fa240a7721d5179b
tagrn <fksk94@gmail.com>
2023-08-06 00:45:30 -0700

    feat: domain modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,10 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LoggerMiddleware } from './middlewares/logger.middleware';
+import { UsersModule } from './users/users.module';
+import { WorkspacesModule } from './workspaces/workspaces.module';
+import { DmsModule } from './dms/dms.module';
+import { ChannelsModule } from './channels/channels.module';
 
 const getThirdPartySecrets = async () => {
   // 서드파티의 환경변수 가져오기. axios 등 사용.
@@ -16,6 +20,10 @@ const getThirdPartySecrets = async () => {
       envFilePath: ['.env', '.env.local', '.env.dev', '.env.prod'],
       load: [getThirdPartySecrets],
     }),
+    UsersModule,
+    WorkspacesModule,
+    DmsModule,
+    ChannelsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
